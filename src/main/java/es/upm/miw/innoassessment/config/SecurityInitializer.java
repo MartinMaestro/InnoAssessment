31739bfab4bd863a354ffad186d8890a4fc5db29
Gonzalo Gutierrez Martin-Maestro <gonzalo.gutierrezm@alumnos.upm.es>
2016-05-21 12:02:36 +0200

    configuración

---
+++ src/main/java/es/upm/miw/innoassessment/config/SecurityInitializer.java
@@ -0,0 +1,7 @@
+package es.upm.miw.innoassessment.config;
+ 
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+ 
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+   //do nothing
+}
