f4742850b36d085a6481166d0c401b8e00aa8536
Miriam <Miriam@192.168.1.131>
2016-05-25 22:47:14 +0200

    change AssesmentLineDao

--- src/main/java/es/upm/miw/innoassessment/data/daos/AssessmentLineDao.java
+++ src/main/java/es/upm/miw/innoassessment/data/daos/AssessmentLineDao.java
@@ -5,12 +5,12 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import es.upm.miw.innoassessment.data.entities.AssessmentLine;
 import es.upm.miw.innoassessment.data.entities.ModelItem;
-import es.upm.miw.innoassessment.data.entities.Questionnarie;
+import es.upm.miw.innoassessment.data.entities.Questionnaire;
 
 public interface AssessmentLineDao extends JpaRepository<AssessmentLine, Integer>, AssessmentLineDaoExtended {
 
 	List<AssessmentLine> findByModel(ModelItem modelItem);
-	List<AssessmentLine> findByQuestionnarie(Questionnarie questionnarie);
+	List<AssessmentLine> findByQuestionnarie(Questionnaire questionnarie);
 
 
 }
