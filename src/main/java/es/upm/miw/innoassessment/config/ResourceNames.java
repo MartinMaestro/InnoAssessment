72cfc3471dc134a52a1108579c91303b087e2dcd
Gonzalo Gutierrez Martin-Maestro <gonzalo.gutierrez.martin.contractor@bbva.com>
2016-05-31 22:20:25 +0200

    fix resource names

--- src/main/java/es/upm/miw/innoassessment/config/ResourceNames.java
+++ src/main/java/es/upm/miw/innoassessment/config/ResourceNames.java
@@ -4,16 +4,16 @@ public class ResourceNames {
 
     public static final String PROPERTIES = "classpath:META-INF/application.properties";
 
-    public static final String ASPECT = "aspect";
+    public static final String ASPECT = "es.upm.miw.innoassessment.aspect";
 
-    public static final String REST_API = "business.api";
+    public static final String REST_API = "es.upm.miw.innoassessment.business.api";
 
-    public static final String CONTROLLERS = "business.controllers";
+    public static final String CONTROLLERS = "es.upm.miw.innoassessment.business.controllers";
 
-    public static final String ENTITIES = "data.entities";
+    public static final String ENTITIES = "es.upm.miw.innoassessment.data.entities";
 
-    public static final String DAOS = "data.daos";
+    public static final String DAOS = "es.upm.miw.innoassessment.data.daos";
 
-    public static final String SERVICES = "data.services";
+    public static final String SERVICES = "es.upm.miw.innoassessment.data.services";
 
 }
