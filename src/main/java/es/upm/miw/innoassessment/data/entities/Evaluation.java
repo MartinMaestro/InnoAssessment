53b8d2ee9e6e0b7e2af9461221c37935eb515876
Gonzalo Gutierrez Martin-Maestro <gonzalo.gutierrez.martin.contractor@bbva.com>
2016-06-05 03:32:54 +0200

    creación de entidad evaluación

---
+++ src/main/java/es/upm/miw/innoassessment/data/entities/Evaluation.java
@@ -0,0 +1,43 @@
+package es.upm.miw.innoassessment.data.entities;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.UniqueConstraint;
+
+@Entity
+@Table(uniqueConstraints={
+		   @UniqueConstraint(columnNames={"questionnaire_id", "product_id", "creationTimeStamp"})
+		})
+public class Evaluation {
+	@Id
+	@GeneratedValue
+	private int id;
+
+	@ManyToOne
+	@JoinColumn
+	private Questionnaire questionnaire;
+
+	@ManyToOne
+	@JoinColumn
+	private Product product;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(nullable = false)
+	private Date creationTimeStamp;
+
+	public Evaluation(Questionnaire questionnaire, Product product, Date creationTimeStamp) {
+		this.questionnaire = questionnaire;
+		this.product = product;
+		this.creationTimeStamp = creationTimeStamp;
+	}	
+
+}
