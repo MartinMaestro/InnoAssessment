4779ab47b509bcb8c1b233c5357842909ec14945
Gonzalo Gutierrez Martin-Maestro <gonzalo.gutierrez.martin.contractor@bbva.com>
2016-06-07 06:21:25 +0200

    pom para desplegar y nueva entidad Productversion

---
+++ src/main/java/es/upm/miw/innoassessment/data/entities/ProductVersion.java
@@ -0,0 +1,29 @@
+package es.upm.miw.innoassessment.data.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class ProductVersion {
+	@Id
+	@GeneratedValue
+	private int id;
+
+	@Column(nullable = false)
+	private String name;	
+
+	@ManyToOne
+	@JoinColumn
+	private Product product;
+
+	public ProductVersion(String name, Product product) {
+		super();
+		this.name = name;
+		this.product = product;
+	}
+	
+}
--- src/main/webapp/WEB-INF/views/jsp/home.jsp
+++ src/main/webapp/WEB-INF/views/jsp/home.jsp
@@ -7,7 +7,7 @@
 <title>Innoassessment</title>
 </head>
 <body>
-<h1>Este es nuestro primer Home - INNOASSESSMENT</h1>
+<h1>- INNOASSESSMENT -</h1>
     <p><a href="<c:url value='/assessmentLine-list'/>">- Lista de Assessment</a></p>
     <p><a href="<c:url value='/dimension-list'/>">- Lista de Dimension</a></p>
     <p><a href="<c:url value='/create-dimension'/>">- Crear una dimension</a></p>
