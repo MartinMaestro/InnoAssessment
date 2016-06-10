5c1cd69884d6bacb377d034505540816f2de06de
MSP <miriam.sanz.pascua@alumnos.upm.com>
2016-06-10 16:59:33 +0200

    eliminacion import

--- src/main/java/es/upm/miw/innoassessment/data/daos/DimensionDao.java
+++ src/main/java/es/upm/miw/innoassessment/data/daos/DimensionDao.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import es.upm.miw.innoassessment.data.entities.Dimension;
-import es.upm.miw.innoassessment.data.entities.Questionnaire;
 
 public interface DimensionDao extends JpaRepository<Dimension, Integer>, DimensionDaoExtended {
 
