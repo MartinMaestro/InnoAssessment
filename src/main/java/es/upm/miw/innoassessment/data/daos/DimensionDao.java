86c3c9313299475c7edd818d42c3e57e8944d6f3
Gonzalo Gutierrez Martin-Maestro <gonzalo.gutierrez.martin.contractor@bbva.com>
2016-06-12 19:42:49 +0200

    buscar dimensiones por cuestionario

--- src/main/java/es/upm/miw/innoassessment/data/daos/DimensionDao.java
+++ src/main/java/es/upm/miw/innoassessment/data/daos/DimensionDao.java
@@ -3,6 +3,7 @@ package es.upm.miw.innoassessment.data.daos;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import es.upm.miw.innoassessment.data.entities.Dimension;
 
@@ -10,6 +11,7 @@ public interface DimensionDao extends JpaRepository<Dimension, Integer>, Dimensi
 
 	List<Dimension> findAllByOrderByIdAsc();
 
-	
+	@Query(value = "SELECT DISTINCT Dimension.id, Dimension.name FROM Dimension JOIN(ModelItem, AssessmentLine, Questionnaire) ON ( Dimension.id = ModelItem.dimension_id AND ModelItem.id = AssessmentLine.modelItem_id AND Questionnaire.id = AssessmentLine.questionnaire_id ) WHERE Questionnaire.id = ?1 ORDER BY Dimension.id", nativeQuery = true)
+	List<Dimension> findByQuestionnaireId(int questionnaireId);
 	
 }
