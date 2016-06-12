be5eb81a2c3a6265341c1a39a5686f5993cef41f
Sanz Pascua <miriam.sanz.pascua@acciona.com>
2016-06-13 01:02:02 +0200

    pruebacommi

--- src/main/java/es/upm/miw/innoassessment/web/QuestionnaireProductPresenter.java
+++ src/main/java/es/upm/miw/innoassessment/web/QuestionnaireProductPresenter.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.ModelAndView;
